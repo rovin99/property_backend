31336cd5f786e8e2dc68b0c243a0f65397942ca6
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2023-12-06 20:24:48 +0530

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -6,15 +6,13 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require('/etc/secrets/config.js')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 let sequelize;
+console.log(config.use_env_variable);
+sequelize = new Sequelize(process.env[config.use_env_variable], config);
+console.log(process.env[config.use_env_variable])
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
 
 fs
   .readdirSync(__dirname)

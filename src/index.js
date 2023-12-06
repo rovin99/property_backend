b63b98c2003c58aae547e5e6e99214b663973311
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2023-12-06 18:25:09 +0530

    removed app.listen

--- src/index.js
+++ src/index.js
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ extended:true }));
 app.use('/api',apiRoutes);
 
 
-app.listen(ServerConfig.PORT,()=>{
-    console.log(`listening on port: ${ServerConfig.PORT}`);
-    Logger.info('successfully started',"root",{msg:"something started"});
+// app.listen(ServerConfig.PORT,()=>{
+//     console.log(`listening on port: ${ServerConfig.PORT}`);
+//     Logger.info('successfully started',"root",{msg:"something started"});
 
     
-})
+// })

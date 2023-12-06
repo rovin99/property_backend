0e131657e0d868ef3b1ff0823de525d73c7dc15a
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2023-12-06 18:19:03 +0530

    config file updated for production

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ const express= require('express');
 const {ServerConfig,Logger}=require('./config');
 const apiRoutes= require('./routes');
 const app = express();
+const cors = require('cors');
+const corsOrigin = "*";
+
+
+
+app.use(cors({
+    origin: corsOrigin,
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use('/api',apiRoutes);
+
 
 app.listen(ServerConfig.PORT,()=>{
     console.log(`listening on port: ${ServerConfig.PORT}`);
--- src/seeders/20231205192935-property.js
+++ src/seeders/20231205192935-property.js
@@ -13,7 +13,54 @@ module.exports = {
      * }], {});
     */
     await queryInterface.bulkInsert('Properties', [
-      
+      {
+        name: 'Sky Heights',
+        location: 'Mumbai',
+        availablefrom: "2022-07-10 11:00:55",
+        price: 70000,
+        type: 'flat',
+        amenities: '3 Bedroom,2 Bathroom,Fully Furnished',
+        carpetArea: '1000 sqft',
+        ownerId: 1,
+        createdAt: new Date('2023-12-05T19:15:16.000Z'),
+        updatedAt: new Date('2023-12-05T19:23:06.000Z'),
+      },
+      {
+        name: 'Dream Land',
+        location: 'Bengaluru',
+        availablefrom: "2022-08-10 11:00:55",
+        price: 90000,
+        type: 'flat',
+        amenities: '3 Bedroom,2 Bathroom,Fully Furnished',
+        carpetArea: '1000 sqft',
+        ownerId: 2,
+        createdAt: new Date('2023-12-05T20:03:16.000Z'),
+        updatedAt: new Date('2023-12-05T20:03:16.000Z'),
+      },
+      {
+        name: 'Galaxy Heights',
+        location: 'Delhi',
+        availablefrom: "2022-09-10 11:00:55",
+        price: 100000,
+        type: 'villa',
+        amenities: '3 Bedroom,2 Bathroom,Fully Furnished',
+        carpetArea: '10000 sqft',
+        ownerId: 2,
+        createdAt: new Date('2023-12-05T20:04:33.000Z'),
+        updatedAt: new Date('2023-12-05T20:04:33.000Z'),
+      },
+      {
+        name: 'Manas Society',
+        location: 'Mumbai',
+        availablefrom: "2022-10-10 11:00:55",
+        price: 100000,
+        type: 'flat',
+        amenities: '3 Bedroom,2 Bathroom,Fully Furnished',
+        carpetArea: '1000sqft',
+        ownerId: 7,
+        createdAt: new Date('2023-12-05T20:07:11.000Z'),
+        updatedAt: new Date('2023-12-05T20:07:11.000Z'),
+      },
     ]);
   },
 
@@ -24,5 +71,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Properties', null, {});
   }
 };
